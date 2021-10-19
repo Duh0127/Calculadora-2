6a3c1379fee2c7a20de7a3ed4d56576433cbce27
Eduardo Toshio Rocha Okubo <eduardo.okubo@etec.sp.gov.br>
2021-10-19 20:43:34 -0300

    Calculadora Final

--- app/src/main/java/com/example/calculadora2/MainActivity.kt
+++ app/src/main/java/com/example/calculadora2/MainActivity.kt
@@ -17,8 +17,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calcular.setOnClickListener{  }
+        binding.calcular.setOnClickListener{ calcularNum() }
     }
+
+    fun calcularNum() {
+
+        val numeroUm = binding.n1.text.toString()
+        val n1 = numeroUm.toDouble()
+
+        val numeroDois = binding.n2.text.toString()
+        val n2 = numeroDois.toDouble()
+
+        val radioSelecionado = binding.GrupoRadioComandos.checkedRadioButtonId
+        val opcaoCalculos = when (radioSelecionado){
+            R.id.RadioSoma -> n1 + n2
+            R.id.RadioMultiplicacao -> n1 * n2
+            R.id.radioDivisao -> n1 / n2
+            else -> n1 - n2
+        }
+
+        val toast = Toast.makeText (this, "Calculo realizado com sucesso", Toast.LENGTH_SHORT).show()
+
+
+        binding.resultado.text = opcaoCalculos.toString()
 
     }
 
+
+
+
+
+
+}
